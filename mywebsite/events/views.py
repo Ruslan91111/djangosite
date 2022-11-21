4e355a00329a36527d9c4ddf4d70f2ee363a3cd7
Ruslan9111 <ruslan_ru911@mail.ru>
2022-11-21 21:20:07 +0500

    Search Bar, update_venue

--- mywebsite/events/views.py
+++ mywebsite/events/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 from .models import Event, Venue
 from .forms import VenueForm
 from django.http import HttpResponseRedirect
+
+
+def update_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    form = VenueForm(request.POST or None, instance=venue)
+    if form.is_valid():
+        form.save()
+        return redirect('list-venues')
+
+    return render(request, 'events/update_venue.html', {'venue': venue, 'form': form, })
+
+
+
+def search_venues(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        venues = Venue.objects.filter(name__contains=searched)
+        return render(request, 'events/search_venues.html', {'searched': searched, 'venues': venues})
+    else:
+        return render(request, 'events/search_venues.html', {})
+
 
 
 def show_venue(request, venue_id):
